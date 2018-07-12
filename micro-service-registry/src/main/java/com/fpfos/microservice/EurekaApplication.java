e23216056b3940c505e08825fbb716b24cc82c6e
root <root@QF-PENGFEIF-01.quark.com>
2018-07-12 16:00:43 +0800

    begin eureka server

--- micro-service-registry/src/main/java/com/fpfos/microservice/EurekaApplication.java
+++ micro-service-registry/src/main/java/com/fpfos/microservice/EurekaApplication.java
@@ -2,14 +2,14 @@ package com.fpfos.microservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * Created by Fu Pengfei on 2018/7/11.
  */
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableEurekaServer
 public class EurekaApplication {
 
     public static void main(String[] args) {

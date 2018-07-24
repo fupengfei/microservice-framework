a01ec037955a4f10c8b3c00df7608662da58336f
fpfos <fpfos@hotmail.com>
2018-07-24 18:19:44 +0800

    add user pom.xml

---
+++ microservice-user/src/main/java/com/fpfos/microservice/v1/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.fpfos.microservice.v1.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by Fu Pengfei on 2018/7/24.
+ */
+@RestController
+public class UserController {
+
+    public Mono<String> login(){
+
+        return null;
+    }
+}
